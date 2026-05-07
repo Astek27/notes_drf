9da687f6b24b3681e88619412557f7c78c1b4187
Astek27 <formov.efim@yandex.ru>
2026-05-07 14:56:12 +0300

    Add note model in apps/notes/models.py

--- apps/notes/models.py
+++ apps/notes/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.core.validators import MaxLengthValidator
 
 # Create your models here.
+class Note(models.Model):
+    title = models.CharField(max_length=100, verbose_name='Заголовок',
+                             help_text='Введите заголовок заметки')
+    content = models.TextField(validators=[MaxLengthValidator(1000)], verbose_name='Содержание',
+                               help_text='Введите содержание заметки')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата изменения')
+
+    class Meta:
+        verbose_name = 'Заметка'
+        verbose_name_plural = 'Заметки'
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.title[:50]
